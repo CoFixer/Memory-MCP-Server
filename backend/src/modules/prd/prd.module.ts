df98effa04c8db541f7dc614c42bcea3d7e15671
Siam Maruf <siammaruf@example.com>
2026-09-21 13:04:31 +0600

    fix(backend): import AuthModule in PrdModule to resolve CombinedAuthGuard deps

--- backend/src/modules/prd/prd.module.ts
+++ backend/src/modules/prd/prd.module.ts
@@ -6,9 +6,10 @@ import { PrdDocument } from '../../database/entities/prd-document.entity';
 import { PrdChunk } from '../../database/entities/prd-chunk.entity';
 import { PrdRelation } from '../../database/entities/prd-relation.entity';
 import { Project } from '../../database/entities/project.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PrdDocument, PrdChunk, PrdRelation, Project])],
+  imports: [TypeOrmModule.forFeature([PrdDocument, PrdChunk, PrdRelation, Project]), AuthModule],
   controllers: [PrdController],
   providers: [PrdService],
   exports: [PrdService],
